6cf8fb228d70f56856c3f7ed082279ee8662534b
Matt Goodall <matt@jdi-associates.com>
2008-07-21 17:25:27 +0100

    Turn Structure into a metaclass for nicer instantiation.
    In the process of doing this I realised that we're really talking about
    attributes instead of fields (so I renamed them) and that an attribute only
    becomes named when it's added to something that needs names (so I moved the
    name to the container).

--- schemaish/__init__.py
+++ schemaish/__init__.py
@@ -1,10 +1,9 @@
 from formencode.validators import *
 
 
-class Field(object):
+class Attribute(object):
 
-    def __init__(self, name, title, description=None, validator=None):
-        self.name = name
+    def __init__(self, title, description=None, validator=None):
         self.title = title
         self.description = description
         self.validator = validator
@@ -15,67 +14,93 @@ class Field(object):
         return self.validator.to_python(value)
 
 
-class String(Field):
+class String(Attribute):
     pass
 
 
-class Integer(Field):
+class Integer(Attribute):
     pass
 
 
-class Float(Field):
+class Float(Attribute):
     pass
 
 
-class Decimal(Field):
+class Decimal(Attribute):
     pass
 
 
-class Date(Field):
+class Date(Attribute):
     pass
 
 
-class Time(Field):
+class Time(Attribute):
     pass
 
 
-class DateTime(Field):
+class DateTime(Attribute):
     pass
 
 
-class Sequence(Field):
+class Sequence(Attribute):
 
     def __init__(self, *a, **k):
-        field = k.pop("field")
+        attr = k.pop("attr")
         super(Sequence, self).__init__(*a, **k)
-        self.field = field
+        self.attr = attr
 
     def validate(self, value):
-        value = [self.field.validate(item) for item in value]
+        value = [self.attr.validate(item) for item in value]
         return super(Sequence, self).validate(value)
 
 
-class Tuple(Field):
+class Tuple(Attribute):
 
     def __init__(self, *a, **k):
-        fields = k.pop("fields")
+        attrs = k.pop("attrs")
         super(Tuple, self).__init__(*a, **k)
-        self.fields = fields
+        self.attrs = attrs
 
     def validate(self, value):
-        value = tuple(field.validate(item) for (field, item) in zip(self.fields, value))
+        value = tuple(attr.validate(item) for (attr, item) in zip(self.attrs, value))
         return super(Tuple, self).validate(value)
 
 
-class Structure(Field):
+class _StructureMeta(type):
+
+    def __new__(cls, name, bases, clsattrs):
+        attrs = []
+        for (name, value) in clsattrs.items():
+            if isinstance(value, Attribute):
+                attrs.append((name, value))
+                del clsattrs[name]
+        clsattrs["attrs"] = attrs
+        return type.__new__(cls, name, bases, clsattrs)
+
+
+class Structure(Attribute):
+
+    __metaclass__ = _StructureMeta
+
+    attrs = []
 
     def __init__(self, *a, **k):
-        fields = k.pop('fields')
+        attrs = k.pop('attrs', None)
         super(Structure, self).__init__(*a, **k)
-        self.fields = fields
+        if attrs is not None:
+            self.attrs = attrs
+
+    def add(self, name, attr):
+        self.attrs.append((name, attr))
+
+    def get(self, name):
+        for (attr_name, attr) in self.attrs:
+            if attr_name == name:
+                return attr
+        raise KeyError(name)
 
     def validate(self, value):
-        value = dict((field.name, field.validate(value[field.name])) for field in self.fields)
+        value = dict((name, attr.validate(value[name])) for (name, attr) in self.attrs)
         return super(Structure, self).validate(value)
 
 
@@ -87,43 +112,43 @@ if __name__ == "__main__":
     class TestString(unittest.TestCase):
 
         def test_validate(self):
-            self.assertEquals(String("string", "String").validate(""), "")
-            self.assertEquals(String("string", "String").validate(None), None)
-            self.assertRaises(Invalid, String("string", "String", validator=NotEmpty()).validate, None)
-            self.assertRaises(Invalid, String("string", "String", validator=NotEmpty()).validate, "")
+            self.assertEquals(String("String").validate(""), "")
+            self.assertEquals(String("String").validate(None), None)
+            self.assertRaises(Invalid, String("String", validator=NotEmpty()).validate, None)
+            self.assertRaises(Invalid, String("String", validator=NotEmpty()).validate, "")
 
     class TestDate(unittest.TestCase):
 
         def test_validate(self):
             today = datetime.date.today()
-            self.assertEquals(Date("date", "Date").validate(None), None)
-            self.assertEquals(Date("date", "Date").validate(today), today)
-            self.assertEquals(Date("date", "Date", validator=NotEmpty).validate(today), today)
-            self.assertRaises(Invalid, Date("date", "Date", validator=NotEmpty).validate, None)
+            self.assertEquals(Date("Date").validate(None), None)
+            self.assertEquals(Date("Date").validate(today), today)
+            self.assertEquals(Date("Date", validator=NotEmpty).validate(today), today)
+            self.assertRaises(Invalid, Date("Date", validator=NotEmpty).validate, None)
 
     class TestSequence(unittest.TestCase):
 
         def test_validate(self):
-            s = Sequence("sequence", "Sequence", field=String("string", "String"))
+            s = Sequence("Sequence", attr=String("string", "String"))
             self.assertEquals(s.validate([]), [])
             self.assertEquals(s.validate(["one", "two"]), ["one", "two"])
-            s = Sequence("sequence", "Sequence", field=String("string", "String"), validator=NotEmpty)
+            s = Sequence("Sequence", attr=String("string", "String"), validator=NotEmpty)
             self.assertEquals(s.validate(["one"]), ["one"])
             self.assertRaises(Invalid, s.validate, [])
-            s = Sequence("sequence", "Sequence", field=String("string", "String", validator=NotEmpty))
+            s = Sequence("Sequence", attr=String("string", "String", validator=NotEmpty))
             self.assertEquals(s.validate([]), [])
             self.assertRaises(Invalid, s.validate, [""])
 
     class TestTuple(unittest.TestCase):
 
         def test_validate(self):
-            t = Tuple("tuple", "Tuple", fields=[String("one", "One"), String("two", "Two")])
+            t = Tuple("Tuple", attrs=[String("One"), String("Two")])
             self.assertEquals(t.validate(tuple()), tuple())
             self.assertEquals(t.validate(("one", "two")), ("one", "two"))
-            t = Tuple("tuple", "Tuple", fields=[String("one", "One"), String("two", "Two")], validator=NotEmpty)
+            t = Tuple("Tuple", attrs=[String("One"), String("Two")], validator=NotEmpty)
             self.assertEquals(t.validate(("one",)), ("one",))
             self.assertRaises(Invalid, t.validate, tuple())
-            t = Tuple("tuple", "Tuple", fields=[String("one", "One", validator=NotEmpty), String("two", "Two")])
+            t = Tuple("Tuple", attrs=[String("One", validator=NotEmpty), String("Two")])
             self.assertEquals(t.validate(("one", "two")), ("one", "two"))
             self.assertEquals(t.validate(("one", "")), ("one", ""))
             self.assertRaises(Invalid, t.validate, ("", ""))
@@ -131,32 +156,57 @@ if __name__ == "__main__":
     class TestStructure(unittest.TestCase):
 
         def test_validate_empty(self):
-            s = Structure("structure", "Structure", fields=[])
+            s = Structure("Structure", attrs=[])
             self.assertEquals(s.validate({}), {})
-            self.assertEquals(s.validate({"notafield": "bleurgh!"}), {})
+            self.assertEquals(s.validate({"notanattr": "bleurgh!"}), {})
 
         def test_validate_extra(self):
-            s = Structure("structure", "Structure", fields=[String("one", "One")])
-            self.assertEquals(s.validate({"one": "un", "notafield": "Hah!"}), {"one": "un"})
+            s = Structure("Structure", attrs=[("one", String("One"))])
+            self.assertEquals(s.validate({"one": "un", "notanattr": "Hah!"}), {"one": "un"})
 
-        def test_validate_fields(self):
-            s = Structure("structure", "Structure", fields=[String("one", "One"), String("two", "Two")])
+        def test_validate_attrs(self):
+            s = Structure("Structure", attrs=[("one", String("One")), ("two", String("Two"))])
             self.assertEquals(s.validate({"one": "un", "two": "deux"}), {"one": "un", "two": "deux"})
             self.assertRaises(KeyError, s.validate, {})
 
         def test_validate_nested(self):
 
-            one = Structure("one", "Structure", fields=[String("a", "A"), String("b", "B")])
-            two = Structure("two", "Structure", fields=[String("a", "A"), String("b", "B")])
-            s = Structure("structure", "Structure", fields=[one, two])
+            one = Structure("One", attrs=[("a", String("A")), ("b", String("B"))])
+            two = Structure("Two", attrs=[("a", String("A")), ("b", String("B"))])
+            s = Structure("Structure", attrs=[("one", one), ("two", two)])
             self.assertEquals(s.validate({"one": {"a": "1a", "b": "1b"}, "two": {"a": "2a", "b": "2b"}}), {"one": {"a": "1a", "b": "1b"}, "two": {"a": "2a", "b": "2b"}})
 
-            s = Structure("structure", "Structure", fields=[
-                Structure("one", "Structure", fields=[String("a", "A", validator=NotEmpty), String("b", "B")]),
+            s = Structure("Structure", attrs=[
+                ("one", Structure("One", attrs=[
+                    ("a", String("A", validator=NotEmpty)),
+                    ("b", String("B")),
+                    ])),
                 ])
             self.assertEquals(s.validate({"one": {"a": "1a", "b": "1b"}}), {"one": {"a": "1a", "b": "1b"}})
             self.assertRaises(Invalid, s.validate, {"one": {"a": "", "b": "1b"}})
             self.assertRaises(Invalid, s.validate, {"one": {"a": None, "b": "1b"}})
+
+        def test_add(self):
+            s = Structure("Structure")
+            s.add("one", String("One"))
+            s.add("two", String("Two"))
+            self.assertEquals(s.validate({"one": "un", "two": "deux"}), {"one": "un", "two": "deux"})
+
+        def test_get(self):
+            one = String("One")
+            s = Structure("Structure", attrs=[("one", one)])
+            self.assertTrue(s.get("one") is one)
+            self.assertRaises(KeyError, s.get, "two")
+
+        def test_meta(self):
+
+            class TestStructure(Structure):
+                one = String("One", validator=NotEmpty)
+                two = String("Two")
+
+            s = TestStructure("Structure")
+            self.assertEquals(s.validate({"one": "One", "two": "Two"}), {"one": "One", "two": "Two"})
+            self.assertRaises(Invalid, s.validate, {"one": "", "two": "Two"})
 
     unittest.main()
 
