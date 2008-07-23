00312d708af440e636763098f42ef6e344ffca49
Matt Goodall <matt@jdi-associates.com>
2008-07-23 10:38:55 +0100

    Be a *lot* more selective about what is imported from formencode.
    FormEncode is full of crap and half the stuff in its validators module really
    aren't anything to do with validation (in our opinion). There's also useful
    validators in its compound module that need including.
    
    Finally, some of the validators are weird so we don't want to encourage their
    use ;-).

--- schemaish/__init__.py
+++ schemaish/__init__.py
@@ -6,6 +6,10 @@ A high-level schema definition package, using formencode for validation.
 # Import all public (or at least often used) names from the schemaish package
 # as well as all FormEncode validators.
 
-from formencode.validators import *
 from schemaish.attr import *
+
+from formencode.validators import Invalid
+from formencode.compound import All, Any
+from formencode.validators import CIDR, Email, Empty, MACAddress, NotEmpty, \
+        OneOf, PlainText, Regex, URL, Wrapper
 
